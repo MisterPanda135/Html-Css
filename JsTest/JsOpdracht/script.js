6475ee8419d455aee5e9fafb902167a4475c32c5
Luca <lucadebruin@gmail.com>
2026-01-15 13:48:13 +0100

    kleine bugs gefixed met de footer

--- JsTest/JsOpdracht/script.js
+++ JsTest/JsOpdracht/script.js
@@ -4,13 +4,14 @@ button.addEventListener("click", waardes);
 let textElement = document.querySelector("#text");
 
 function waardes() {
-    let array = [];
-    for (var i = 0; i < 5; i++) {
+    const array = [];
+    for (let i = 0; i < 5; i++) {
         array.push(Math.floor(Math.random() * 6) + 1);
     }
     console.log(array);
-    for (var i = 0; i < array.length; i++) {
-        textElement.textContent += " " + array[i];
-    }
+    // for (let i = 0; i < array.length; i++) {
+    //     textElement.textContent += " " + array[i];
+    // }
+    textElement.textContent = array;
 
 }
